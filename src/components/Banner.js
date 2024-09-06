f2daab966c2deec6d95ff6e5300f6e8de6353958
Alishabab62 <shababali672@gmail.com>
2024-09-06 23:40:33 +0530

    ui updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Title from './Title'
 import profile from "../assests/profile.jpg"
+ // Update the path to your resume file
 
 const Banner = () => {
+  const url = "https://drive.google.com/file/d/1i9WIuZEQUXBKKHurEesGxA8p2d0Q_Agg/view?usp=drive_link"
   return (
     <div className='bg-gradient-to-r from-gray-900 to-gray-800 text-white md:flex p-6 md:p-10 w-5/6 mx-auto my-8 rounded-lg shadow-lg' id='home'>
       <div className='md:w-2/3 w-full flex flex-col justify-center'>
@@ -10,9 +12,11 @@ const Banner = () => {
         <p className='py-4 text-lg md:text-2xl text-gray-300'>
           I'm a self-taught, passionate web developer 💪 creating captivating interfaces and constantly expanding skills 💻. Excited for what lies ahead! 😎
         </p>
-        <button className='bg-yellow-500 hover:bg-yellow-600 text-black px-6 py-3 rounded-lg text-lg font-semibold transition duration-300'>
-          Download Resume
-        </button>
+        <a href={url} target='blank' download="Resume.pdf">
+          <button className='bg-yellow-500 hover:bg-yellow-600 text-black px-6 py-3 rounded-lg text-lg font-semibold transition duration-300'>
+            Download Resume
+          </button>
+        </a>
       </div>
       <div className='md:w-1/3 w-full flex items-center justify-center mt-6 md:mt-0'>
         <img className='rounded-full h-72 md:h-80 border-4 border-yellow-500' src={profile} alt='profile'/>
